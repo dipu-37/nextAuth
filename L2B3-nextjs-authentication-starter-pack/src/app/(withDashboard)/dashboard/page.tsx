a3b7b8f75716297f83f011e1a4e777d525e33bd0
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-08-13 23:59:33 +0600

    first commit

---
+++ L2B3-nextjs-authentication-starter-pack/src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  return (
+    <div>
+      {
+        session?.user &&
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Login User Email : {session?.user?.email}
+          </h1>
+          <Image
+            src={session?.user?.image}
+            width={100}
+            height={100}
+            alt="user Image"
+            className="mx-auto rounded-full mt-5"
+          ></Image>
+        </>
+        
+      }
+    </div>
+  );
+};
+
+export default DashboardPage;
---
+++ L2B3-nextjs-authentication-starter-pack/src/utils/authOptions.ts
@@ -0,0 +1,21 @@
+import { Secret } from './../../node_modules/next-auth/src/jwt/types';
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+import  { NextAuthOptions } from "next-auth"
+
+
+export const authOptions :NextAuthOptions= {
+  providers: [
+   GitHubProvider({
+    clientId: process.env.GITHUB_ID as string,
+    clientSecret: process.env.GITHUB_SECRET as string
+  }),
+   GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+  }),
+  ],
+  secret : process.env.NEXTAUTH_SECRET,
+}
+
