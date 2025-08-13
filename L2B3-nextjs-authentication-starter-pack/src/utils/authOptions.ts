35a941c3dff9fa6df6f18d87d817137fe849be1a
Razibul Hasan Dipu <razibul1105nstu@gmail.com>
2025-08-14 02:33:09 +0600

    register user

--- L2B3-nextjs-authentication-starter-pack/src/utils/authOptions.ts
+++ L2B3-nextjs-authentication-starter-pack/src/utils/authOptions.ts
@@ -16,6 +16,9 @@ export const authOptions :NextAuthOptions= {
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
   }),
   ],
+  pages : {
+    signIn:"/login"
+  },
   secret : process.env.NEXTAUTH_SECRET,
 }
 
